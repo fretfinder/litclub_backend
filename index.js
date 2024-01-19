ce1acca4cb4bef76f01312c8089689f47f449b6b
fretfinder <124775242+fretfinder@users.noreply.github.com>
2024-01-19 11:26:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.use(cors(
     }
 ));
 
-const book = require("./books");
-const account = require("./accounts");
+const book = require("./models/books");
+const account = require("./models/accounts");
 
 
 app.post(`/auth` , async function(req,res){
